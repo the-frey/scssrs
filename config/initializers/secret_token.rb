bfe37a6d37b8323b56164ee15d6b34e7150a3247
Alex Lynham <acelynham@hotmail.com>
2014-03-04 23:41:38 +0000

    Twiddled with the app to get it running on heroku.

--- config/initializers/secret_token.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Scssrs::Application.config.secret_token = 'a6d9214dcaf5f1abdb055f166caf59687f5688fae2631e789a32fa5bd7a0cdca5a8ee4446a86acd8d8e8b50d600f1ef2c23006336baaa07755e9d757d7f5fea3'
-
