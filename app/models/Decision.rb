023eb2d4171ac2d35c02a21ae440963b820043c9
Alex Lynham <acelynham@hotmail.com>
2014-03-06 01:18:03 +0000

    Slightly intelligent AI added.

--- app/models/Decision.rb
+++ app/models/Decision.rb
@@ -8,17 +8,17 @@ class Decision < ActiveRecord::Base
   # In the model, though we don't persist the result
   def calculate_winner(ai_move, player_move)
     @player_wins = nil
-    if ai_move == 0  && player_move == 1
+    if ai_move == 0 && player_move == 1 # AI rock, player paper
       @player_wins = true
-    elsif ai_move == 0  && player_move == 2
+    elsif ai_move == 0 && player_move == 2 # AI rock, player scssrs
       @player_wins = false
-    elsif ai_move == 1  && player_move == 0
+    elsif ai_move == 1 && player_move == 0 # AI paper, player rock
       @player_wins = false
-    elsif ai_move == 1  && player_move == 2
+    elsif ai_move == 1 && player_move == 2 # AI paper, player scssrs
       @player_wins = true
-    elsif ai_move == 2  && player_move == 0
+    elsif ai_move == 2 && player_move == 0 # AI scssrs, player rock
       @player_wins = true
-    elsif ai_move == 2  && player_move == 1
+    elsif ai_move == 2 && player_move == 1 # AI scssrs, player paper
       @player_wins = false
     end
     return @player_wins
@@ -30,11 +30,12 @@ class Decision < ActiveRecord::Base
     historic_plays = []
     historic_plays = Decision.where('ip = ?', ip_address).limit(100).order(:created_at)
 
-    logger.debug "#{historic_plays.inspect}"
-
-    if historic_plays == []
+    # Generate randomly if less than five moves or empty
+    if historic_plays == [] || historic_plays.length < 5
       play = simple_ai_move
-    else
+    elsif rand(2) == 0 # Inject some randomness
+      play = simple_ai_move
+    else # Let's predict some player behaviour
       play = complex_ai_move(historic_plays)
     end
 
@@ -49,12 +50,53 @@ class Decision < ActiveRecord::Base
   # Tries to find a simple pattern
   def complex_ai_move(historic_plays) 
     play = nil
-    last_five = historic_plays[1..5]
-    last_four = historic_plays[1..4]
-    last_three = historic_plays[1..3]
+
+    # last_five = historic_plays[1..5] second pass should try this first
+    # last_four = historic_plays[1..4] then this
+    
+    historic_plays_string = []
+    historic_plays.each { |hp| historic_plays_string << hp.player_choice }
+    historic_plays_string = historic_plays_string.join()
+
+    last_three = []
+    historic_plays[1..3].each { |lt| last_three << lt.player_choice }
+    last_three_string = last_three.join()
+
+    play = match_pattern_and_calculate(last_three_string, historic_plays_string)
 
     play ||= simple_ai_move
     play
   end
 
+  # This method finds the string in the haystack and compiles an array of subsequent moves to the matched pattern
+  def match_pattern_and_calculate(matcher, haystack)
+    possible_moves = []
+    continue = true
+
+    until continue == false
+      str = Regexp.new(matcher)
+      index = str =~ haystack
+      if index != nil
+        possible_moves << haystack[(index - 1)]
+        haystack = haystack[(index + 1)..-1]
+      else
+        continue = false
+      end
+    end
+
+    play = most_common_subsequent_move(possible_moves) unless possible_moves == []
+    play ||= nil
+  end
+
+  # This method finds the most common move from the subsequent moves array
+  def most_common_subsequent_move(possible_moves)
+    occurrences = {}
+
+    possible_moves.each do |move|  
+      occurrences[move].nil? ? occurrences[move] = 1 : occurrences[move] += 1
+    end
+
+    most_frequent = occurrences.max[0] unless occurrences == {}
+    most_frequent.to_i unless most_frequent.nil?
+  end
 end
